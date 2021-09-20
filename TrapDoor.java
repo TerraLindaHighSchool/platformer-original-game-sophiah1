428d873d32bd3b65e233d2634175910df9681c02
sophia <sh01806@srcs.org>
2021-09-20 15:45:50 -0700

    Day 2 done

---
+++ TrapDoor.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TrapDoor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TrapDoor extends Obstacle
+{
+    /**
+     * Act - do whatever the TrapDoor wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
