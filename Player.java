3717413b995e0773d839fbd709afd92d53646224
sophia <sh01806@srcs.org>
2021-09-28 14:30:33 -0700

    complete

--- Player.java
+++ Player.java
@@ -7,13 +7,28 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class Player extends Actor
-{
+{   private Health[] health;
+    private Powerup[] powerup;
+    private int healthCount;
+    private int speed;
+    private int walkIndex;
+    private int frame;
+    private float yVelocity;
+    private boolean isWalking;
+    private boolean isJumping;
+    private boolean isFacingLeft;
+    private final GreenfootImage[] WALK_ANIMATION;
+    private final GreenfootImage STANDING_IMAGE;
+    private final float JUMP_FORCE;
+    private final float GRAVITY;
+    private final class NEXT_LEVEL;
+    private final Greenfootsound MUSIC;
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        
     }
 }
